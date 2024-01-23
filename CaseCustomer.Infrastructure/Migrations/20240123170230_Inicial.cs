897b62d87e50f073ec57ec3397b34ab20a3f1ee2
Marcio Aurelio <mferreira@dacorp.com.br>
2024-01-23 14:09:26 -0300

    Alt05_CorrigeDoc

---
+++ CaseCustomer.Infrastructure/Migrations/20240123170230_Inicial.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CaseCustomer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Documentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DocumentoNumero = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClienteAtivo = table.Column<bool>(type: "bit", maxLength: 3, nullable: false),
+                    DocumentoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clientes_Documentos_DocumentoId",
+                        column: x => x.DocumentoId,
+                        principalTable: "Documentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Documentos",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "CPF" },
+                    { 2, "RG" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_DocumentoId",
+                table: "Clientes",
+                column: "DocumentoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Documentos");
+        }
+    }
+}
---
+++ CaseCustomer.Infrastructure/Migrations/20240123170453_PopulaClientes.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CaseCustomer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaClientes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Clientes(Nome,DataNascimento,Endereco,DocumentoNumero,DataCadastro,ClienteAtivo,DocumentoId) " +
+            "VALUES('João da Silva','1978-04-29','Av.Mogi nº 1','27614673883','2024-01-22',1,1)");
+
+            migrationBuilder.Sql("INSERT INTO Clientes(Nome,DataNascimento,Endereco,DocumentoNumero,DataCadastro,ClienteAtivo,DocumentoId) " +
+            "VALUES('Maria do Bairro','1981-04-12','Rua da Amora nº 1000','291689462','2024-01-22',1,2)");
+
+            migrationBuilder.Sql("INSERT INTO Clientes(Nome,DataNascimento,Endereco,DocumentoNumero,DataCadastro,ClienteAtivo,DocumentoId) " +
+            "VALUES('Tyna Tunner','1990-05-01','Av.Cruzeiro do Sul nº 260','123456789','2024-01-22',1,1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
